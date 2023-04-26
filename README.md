256b68f5d4a9816891ee7385b3200c069ab0b892
Jennifer Eckenrode <j.eckenrode@me.com>
2023-04-26 00:30:38 -0500

    Created repo, cloned to local, created files, wrote starter code for files, installed inquirer for node.js, added a screenshot for testing in README.md

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# readmeGenerator
+# Project Description test
+
+## License function generateLicense(license) {
+    if (license === "MIT") {
+        return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+
+    } else if (license === "Apache 2.0") {
+        return "[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+
+    } else if (license === "ISC") {
+        return "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
+
+    } else if (license === "BSD") {
+        return "[![License: BSD](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+
+    } else {
+        return ""
+    }
+}(license) 
+
+### A third-level heading
---
+++ index.mjs
@@ -0,0 +1,58 @@
+import inquirer from 'inquirer';
+import fs from "fs/promises"; 
+
+
+let { description, license } = await inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'description',
+            message: "Write a description of your project: ",
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Which License for your Github repository?',
+            choices: ['MIT', 'Apache 2.0', 'ISC', 'BSD'],
+
+        },
+    ])
+
+let readmeText = `# Project Description ${description}
+
+## License ${generateLicense}(license) 
+
+### A third-level heading`
+
+
+fs.writeFile("README.md", readmeText);
+
+function generateLicense(license) {
+    if (license === "MIT") {
+        return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+
+    } else if (license === "Apache 2.0") {
+        return "[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+
+    } else if (license === "ISC") {
+        return "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
+
+    } else if (license === "BSD") {
+        return "[![License: BSD](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+
+    } else {
+        return ""
+    }
+}
+
+fs.writeFile("README.md", readmeText, function (err) {
+    if (err) {
+        console.log("An error occurred while writing to README.md:", err);
+    } else {
+        console.log("README.md was saved successfully!");
+    }
+});
+ 
+
+
+//console.log(description);
